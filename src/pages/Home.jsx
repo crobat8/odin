0a8d4b9439c689fad9c5603f3b14f178fd75c68a
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-08-09 19:21:32 -0700

    got login and sign up working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Create from '../componets/Create';
 import Friends from '../componets/Friends';
 import Settings from '../componets/Settings';
 
-import logo from '../img/partyuplogo.png'
+import Clearlogo from '../img/ClearBackLogo.png'
 import { auth } from '../firebase'
 import {signOut} from "firebase/auth"
 import { AuthContext } from '../context/AuthContext';
@@ -20,94 +20,85 @@ import Review from '../componets/Review';
 
 const Home = () =>{ 
     
-    const [page,setPage] = useState(1);
+    const [page,setPage] = useState(0);
     const{currentUser} = useContext(AuthContext);
 
     let iconStyles = { color: "#e2f1ff", fontSize: "1.5em" ,padding:"5px"};
     
-    
-
     function HandleSwap(){
-        
-        if(page === 1){
-            return <Parties/>;
-        }else if(page === 2){
-            return <Create/>
-        }else if(page === 3){
-            return <Friends/>
-        }else if(page === 4){
-            return <Settings/>
-        }else if(page === 5){
-            return <Review/>
-        }else {
-            return (
-            <div style={{"min-height": "1000px","textAlign":"center"}}>
-                <h1 style={{"backgroundColor":"#00b2be",
-                            "textAlign":"center",
-                            "padding":"20px",
-                            "borderRadius":"20px"}}>
-                    select a page to go to
-                </h1>
-            </div>
-            )
-        }
-
+      if(page === 1){
+        return <h1>
+          question page
+        </h1>;
+      }else if(page === 2){
+        return <h1>
+          create page
+        </h1>;
+      }else if(page === 3){
+        return <h1>
+          practice quiz
+        </h1>;
+      }else{
+        return (
+          <div style={{"min-height": "1000px","textAlign":"center"}}>
+            <h1 style={{"backgroundColor":"#00b2be",
+                        "textAlign":"center",
+                        "padding":"20px",
+                        "borderRadius":"20px"}}>
+              select a page to go to
+            </h1>
+          </div>
+        )
+      }
     }
     
     const disableButtonHandler = (x)=>{
-        alert(x)
+      alert(x)
     }
 
     return (
         <div className="home" >
-            logged in
-            {/* <header className="topBar" >
-                
-                <img src={logo} alt='logo' onClick={()=>setPage(1)} />
-                
-
+            <header className="topBar" >
+                <img src={Clearlogo} alt='logo' /> 
                 <ul className="selectors"  >
                     <button onClick={()=>setPage(1)}>
-                        Parties
+                        questions
                     </button>
                     {currentUser.uid == "BqJmqRQc2gQnSoHZVwoH8Q99uya2"
                     ?
                     <button onClick={()=>disableButtonHandler("Create is diabled on guest account ")}>
-                        Create
+                        create
                     </button>
                     :
                     <button onClick={()=>setPage(2)}>
-                        Create
+                        create
                     </button>
                     }
                     
                     {currentUser.uid == "BqJmqRQc2gQnSoHZVwoH8Q99uya2"
                     ?
                     <button onClick={()=>disableButtonHandler("Friends are diabled on guest account ")}>
-                        Friends
+                        quiz page
                     </button>
                     :
                     <button onClick={()=>setPage(3)}>
-                        Friends
+                        quiz page
                     </button>
                     }
                     
                 </ul>
-                <div className='user' onClick={()=>setPage(4)}>
-                    <img src={currentUser.photoURL} alt="" width={"100%"}/>
-                    
+                <div className='logoutarea'>
                     <button onClick={()=>signOut(auth)}className='logout'>logout</button>
-                    <h4>
-                        name:{currentUser.displayName}
-                    </h4>
                 </div>
                 
-                
             </header>
-            <main className="page" >
+                
+                
+            
+             <main className="page" >
                 <HandleSwap/>
             </main>
-            <footer className="information" >
+            {/*<footer className="information" >
                 <div className='infoTop'>
                     <div className='left'>
                         <h3>
